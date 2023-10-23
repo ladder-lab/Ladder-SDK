cde327c60bbc8118e3b60d530eb1154f336b1f3f
devLadder <dev@ladder.top>
2023-10-23 14:59:17 +0800

    feature: add bsc chain

--- src/web3/addresses.ts
+++ src/web3/addresses.ts
@@ -2,7 +2,8 @@ import { SupportChain } from "./type"
 
 export const RouteErc721Address: Record<string, string> = {
     [SupportChain.Sepolia]: "0x146ADA3E3A615CAdEBC06C1b02cBE3D040DcC3A0",
-    [SupportChain.Polygon]: "0xf434274C78CF5567D47D765420CDbf552d1C1067"
+    [SupportChain.Polygon]: "0xf434274C78CF5567D47D765420CDbf552d1C1067",
+    [SupportChain.Binance]: "0xb59a8c714515793bDB6Ae7614499ee33b2F2802d"
 }
 
 export const RouteErc1155Address: Record<string, string> = {
@@ -12,5 +13,6 @@ export const RouteErc1155Address: Record<string, string> = {
 
 export const USDT: Record<string, string> = {
     [SupportChain.Sepolia]: "0x85eDB7A0cbAcf5BD641e0FF5D6270bEf9C72Bd6B",
-    [SupportChain.Polygon]: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"
+    [SupportChain.Polygon]: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+    [SupportChain.Binance]: "0x55d398326f99059fF775485246999027B3197955"
 }
